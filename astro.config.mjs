3deff3fa37ff3e65018290e09ee051f609e75776
AbrahamVM2001 <abrahamveram2001@outlook.com>
2025-10-05 14:50:21 -0600

    fix(main) update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  devToolbar: {
+    enabled: false
+  },
   server: {
     allowedHosts: ['codehub.devabraham.com'],
     host: true,
